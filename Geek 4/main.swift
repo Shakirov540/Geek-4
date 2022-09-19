7550b2a50e153ee328703a57f3bb02f1c4dd58ce
Aibek <ashakirov981@gmail.com>
2022-09-19 18:38:37 +0600

    Initial Commit

---
+++ Geek 4/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  Geek 4
+//
+//  Created by Айбек Шакиров on 19/9/22.
+//
+
+import Foundation
+
+for item in 0...10{
+    print(item)
+}
+
+for item in 60..<71{
+    print(item)
+}
+
+for symbol in "ABCDEF"{
+    print(symbol)
+}
